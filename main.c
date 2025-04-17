7fb148e135bd7d8bce4d2668ea8d1779a710d419
Alfredo-Aristizabal <divinebizero@gmail.com>
2025-04-17 12:21:41 -0400

    pointers-to-pointers

--- main.c
+++ main.c
@@ -2,4 +2,14 @@
 
 int main()
 {
+    int n = 30;
+    int *p1 = &n;
+    int **p2 = &p1;
+    int ***p3 = &p2;
+    printf("The value of n is %p\n", (void *)p1);
+    printf("The value of n from p1 is %d\n", *p1);
+    printf("The value of p2 is %p\n", (void *)p2);
+    printf("The value of p1 from p2 is %d\n", **p2);
+    printf("The value of n from p2 is %d\n", **p2);
+    printf("The value of p2 from p3 is %d", ***p3);
 }
