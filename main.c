f8c2ddba99ce8015bd86063d9e788c18d1be5e8b
Alfredo-Aristizabal <divinebizero@gmail.com>
2025-05-02 15:05:03 -0400

    open file

--- main.c
+++ main.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#define SIZE 256
 int main()
 {
+    FILE *fptr;
+    char chr;
+    char buff[SIZE];
+
+    fptr = fopen("./file-to-read.txt", "r");
+    if (fptr == NULL)
+    {
+        fprintf(stderr, "Can not read the file");
+        exit(EXIT_FAILURE);
+    }
+
+    fgets(buff, SIZE, fptr);
+    printf("%s\n", buff);
+
+    fptr = fopen("./file-to-read.txt", "r");
+
+    while ((chr = fgetc(fptr)) != EOF)
+    {
+        printf("%c", chr);
+    }
+    fclose(fptr);
+    exit(EXIT_SUCCESS);
 }
