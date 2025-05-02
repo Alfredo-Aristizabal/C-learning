d8754b31c3244466e87ee63dace6e1984ab75d49
Alfredo-Aristizabal <divinebizero@gmail.com>
2025-05-02 13:53:19 -0400

    .

--- main.c
+++ main.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define EXIT_FAILURE 1
 
 int main()
 {
+    int n = 5;
+    int *ptr = maalloc(n * sizeof *ptr);
+
+    if (ptr == NULL)
+    {
+        return EXIT_FAILURE;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        ptr[i] = i;
+        printf("%d", ptr[i]);
+    }
+    printf("\n");
+
+    ptr = realloc(ptr, 2 * n * sizeof *ptr);
+
+    if (ptr == NULL)
+    {
+        return EXIT_FAILURE;
+    }
 }
